1d905378912918560119b42aaa50f9d330970fe0
Silk Rose <silkrose@love-tolerance.com>
2023-01-17 01:07:28 -0500

    implement load characters and find files, add Ponk

--- src/mane.ts
+++ src/mane.ts
@@ -3,6 +3,7 @@ import fs from "fs";
 import path from "path";
 
 type Character = z.infer<typeof character_validator>;
+type NPC = Record<string, Character>;
 
 const character_validator = z.object({
   name: z.string(),
@@ -15,5 +16,42 @@ const character_validator = z.object({
   inventory: z.array(z.tuple([z.string(), z.number()])),
 });
 
-let rarity: Character = JSON.parse(fs.readFileSync(path.resolve("./game_data/character/rarity.json")).toString());
-console.log(character_validator.parse(rarity));
+function mane() {
+  let characters = load_characters();
+  console.log(characters["pinkie_pie"]);
+}
+
+function load_characters() {
+  let character_files = find_files_in_dir(
+    path.resolve("./game_data/character/"),
+    ".json"
+  );
+  let characters_: NPC = {};
+  for (let file of character_files) {
+    let name = file.split("/").pop()?.split(".")[0];
+    let read_file = fs.readFileSync(file, { encoding: "utf-8" });
+    let json = JSON.parse(read_file);
+    let validated_json = character_validator.parse(json);
+    characters_[`${name}`] = validated_json;
+  }
+  return characters_;
+}
+
+function find_files_in_dir(startPath: string, filter: string) {
+  let results: string[] = [];
+  if (!fs.existsSync(startPath)) {
+    console.log("no dir ", startPath);
+    throw Error;
+  }
+  let files = fs.readdirSync(startPath);
+  for (let i = 0; i < files.length; i++) {
+    let filename = path.join(startPath, files[i]);
+    let stat = fs.lstatSync(filename);
+    if (stat.isFile() && filename.indexOf(filter) >= 0) {
+      results.push(filename);
+    }
+  }
+  return results;
+}
+
+mane();
