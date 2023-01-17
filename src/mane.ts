cf9ea7be90b9c5edeadca8a3a1a9a5cb27f65ed4
Silk Rose <silkrose@love-tolerance.com>
2023-01-17 05:31:02 -0500

    initial create character function

--- src/mane.ts
+++ src/mane.ts
@@ -1,9 +1,16 @@
 import z from "zod";
 import fs from "fs";
 import path from "path";
+import * as readline from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
 
 type Character = z.infer<typeof character_validator>;
+type Name = z.infer<typeof name_validator>;
+type Age = z.infer<typeof age_validator>;
 type NPC = Record<string, Character>;
+
+const name_validator = z.string().min(2).max(32);
+const age_validator = z.number().min(18).max(99);
 
 const character_validator = z.object({
   name: z.string(),
@@ -16,9 +23,45 @@ const character_validator = z.object({
   inventory: z.array(z.tuple([z.string(), z.number()])),
 });
 
-function mane() {
+async function mane() {
+  await create_character();
   let characters = load_characters();
   console.log(characters["pinkie_pie"]);
+}
+
+async function create_character() {
+  let name = await get_name();
+  let age = await get_age();
+  console.log(name, age);
+}
+
+async function get_name() {
+  let name: Name = await read_line("What is you're name?");
+  let validated_name = name_validator.safeParse(name);
+  if (validated_name.success) {
+    return name.trim()
+ } else {
+    console.log("Please provilde a name between 2 and 32 characters.");
+    await get_name();
+ }
+}
+
+async function get_age() {
+  let age: Age = parseInt(await read_line("How old are you?"));
+  let validated_age = age_validator.safeParse(age);
+  if (validated_age.success) {
+    return age
+ } else {
+    console.log("Please provilde an age between 18 and 99.");
+    await get_age();
+ }
+}
+
+async function read_line(text: string) {
+  const response = readline.createInterface({ input, output });
+  const answer = await response.question(`${text} `);
+  response.close();
+  return answer;
 }
 
 function load_characters() {
