7fc2930c84e2c9302a233896124fa953b04efee1
SilkRose <johnmloos1995@gmail.com>
2023-02-21 17:45:35 +0000

    Deploying to gh-pages from @ SilkRose/LibrePony@f52e30bbc60ea30e0cbad9962fb70679ddd7727a 🚀

--- mane.js
+++ mane.js
@@ -38,6 +38,7 @@ function mane() {
             append_element(player.species.sub_race.toString());
         }
         append_element(player.gender);
+        yield get_best_pony();
     });
 }
 function append_element(element) {
@@ -198,7 +199,10 @@ function get_age() {
 function get_species() {
     return __awaiter(this, void 0, void 0, function* () {
         append_element(`What species are you?)`);
-        let race = yield read_line_radial([pony, ...nonpony_species]);
+        let race = yield read_line_radial([
+            pony,
+            ...nonpony_species,
+        ]);
         if (race === pony) {
             append_element(`What pony race are you?`);
             return {
@@ -218,43 +222,68 @@ function get_gender() {
         return gender;
     });
 }
-/*
-async function get_best_pony() {
-  const question = "Who is best Pony? :";
-  const answer = "Pinkie Pie";
-  rl.emitKeypressEvents(process.stdin);
-  if (process.stdin.isTTY) process.stdin.setRawMode(true);
-  process.stdin.write(question);
-  let i = 0;
-  while (i < answer.length) {
-    i = await assert_best_pony(question, answer, i);
-  }
-  const question2 = `Please confirm that ${answer} is indeed best pony. :`;
-  const answer2 = "Yes!";
-  process.stdin.write("\n" + question2);
-  i = 0;
-  while (i < answer2.length) {
-    i = await assert_best_pony(question2, answer2, i);
-  }
-  console.log();
-  process.stdin.setRawMode(false);
-}
-
-function assert_best_pony(question: string, answer: string, i: number) {
-  return new Promise<number>((res) => {
-    process.stdin.once("keypress", (_, key) => {
-      //if (key && key.name == "q") process.exit();
-      let rv;
-      if (key.name === "backspace" || key.name === "delete") rv = i - 1;
-      else rv = i + 1;
-      if (rv < 0) rv = 0;
-      let text = question + answer.slice(0, rv);
-      process.stdout.write("\r");
-      process.stdout.write(" ".repeat(process.stdout.columns));
-      process.stdout.write("\r");
-      process.stdout.write(text);
-      res(rv);
+function get_best_pony() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const question = "Who is best Pony?";
+        const answer = "Pinkie Pie";
+        append_element(question);
+        let i = 0;
+        while (i < answer.length) {
+            i = yield assert_best_pony(answer, i);
+        }
+        const game_content = document.getElementById("game_content");
+        game_content.removeChild(game_content.lastChild);
+        const question2 = `Please confirm that ${answer} is indeed best pony.`;
+        const answer2 = "Yes!";
+        append_element(question2);
+        i = 0;
+        while (i < answer2.length) {
+            i = yield assert_best_pony(answer2, i);
+        }
+        game_content.removeChild(game_content.lastChild);
     });
-  });
 }
- */
+function assert_best_pony(answer, i) {
+    return __awaiter(this, void 0, void 0, function* () {
+        read_line_text_override(answer.slice(0, i), "best_pony");
+        return new Promise((res) => {
+            var _a;
+            (_a = document.getElementById("input")) === null || _a === void 0 ? void 0 : _a.addEventListener("keydown", (key) => {
+                let rv;
+                if (key.key === "backspace" || key.key === "delete")
+                    rv = i - 1;
+                else
+                    rv = i + 1;
+                if (rv < 0)
+                    rv = 0;
+                res(rv);
+            });
+        });
+    });
+}
+function read_line_text_override(value, name) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const new_element = document.createElement("p");
+        new_element.innerHTML = `
+  <div id="input_field">
+    <input type="text" id="input" name="${name}" placeholder="Enter response..." value="${value}">
+    <button id="submit">Enter</button>
+  </div>`;
+        const game_content = document.getElementById("game_content");
+        if (document.getElementById("input_field") === null) {
+            game_content.appendChild(new_element);
+        }
+        else {
+            document.getElementById("input_field").outerHTML = new_element.innerHTML;
+        }
+        new_element.scrollIntoView();
+        const input = document.getElementById("input");
+        input.focus();
+        input.value = value;
+        input.setSelectionRange(value.length, value.length);
+    });
+}
+// TODO:
+// finish/fix best pony.
+// make best pony enter button only appear when the input value matches the answer.
+// make enter button go away if they press backspace and the value matches the answer.
