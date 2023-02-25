85244d14c0c9a83f1f871864d0d1125f58311b8a
Silk Rose <silkrose@love-tolerance.com>
2023-02-25 01:30:00 -0500

    switch radial input to css has selector

--- src/mane.ts
+++ src/mane.ts
@@ -330,29 +330,31 @@ function create_radial_input_field(options: readonly string[]) {
   const new_element = document.createElement("div");
   new_element.id = "input_radial";
   new_element.className = "content";
-  new_element.appendChild(create_radio_element(options[0], true));
-  new_element.appendChild(create_label_element(options[0]));
+  new_element.appendChild(create_label_with_radial_element(options[0], true));
   for (let i = 1; i < options.length; i++) {
-    new_element.appendChild(create_radio_element(options[i]));
-    new_element.appendChild(create_label_element(options[i]));
+    new_element.appendChild(create_label_with_radial_element(options[i]));
   }
   new_element.appendChild(create_button_element("submit", "Enter"));
   return new_element;
 }
 
-function create_radio_element(value: string, checked?: boolean) {
-  const radio = document.createElement("input");
-  radio.type = "radio";
-  radio.id = value;
-  radio.name = "radial";
-  radio.value = value;
-  if (checked) radio.checked = true;
-  return radio;
+function create_label_with_radial_element(text: string, checked?: boolean) {
+  const label = create_label_element(text);
+  label.prepend(create_radio_element(text, checked))
+  return label;
 }
 
 function create_label_element(text: string) {
   const label = document.createElement("label");
-  label.setAttribute("for", text);
   label.innerText = text;
   return label;
+}
+
+function create_radio_element(value: string, checked?: boolean) {
+  const radio = document.createElement("input");
+  radio.type = "radio";
+  radio.name = "radial";
+  radio.value = value;
+  if (checked) radio.checked = true;
+  return radio;
 }
