aeeb40877f3c2d8e5dca8bc896c5b3bcd0a1135b
Meadowsys <blazeykirin@gmail.com>
2023-03-01 03:22:17 -0800

    use sass, reimplement theme thing with most styles in css

--- src/theme.ts
+++ src/theme.ts
@@ -1,69 +1,17 @@
 enum Theme {
-  Light = "light",
-  Dark = "dark",
-  None = "none",
+  Light = "theme-light",
+  Dark = "theme-dark",
+  None = "theme-none",
 }
 
-type ThemeSet = {
-  text: string;
-  background: string;
-  foreground: string;
-  shadow: string;
-  border: string;
-  placeholder: string;
-  input_background: string;
-  button_background: string;
-  button_hover_background: string;
-  mane_accent_inactive: string;
-  mane_accent_active: string;
-};
-
-const light_theme = {
-  text: "#000",
-  background: "#f2f2f2",
-  foreground: "#fff",
-  shadow: "#ccc",
-  border: "#a5a5a5",
-  placeholder: "#555",
-  input_background: "#f2f2f2",
-  button_background: "#e9e9ed",
-  button_hover_background: "#d0d0d7",
-  mane_accent_inactive: "#a5a5a5",
-  mane_accent_active: "#eb458b",
-};
-
-const dark_theme = {
-  text: "#fff",
-  background: "#000",
-  foreground: "#0d0d0d",
-  shadow: "#444",
-  border: "#5a5a5a",
-  placeholder: "#aaaaaa",
-  input_background: "#0d0d0d",
-  button_background: "#161612",
-  button_hover_background: "#2f2f28",
-  mane_accent_inactive: "#5a5a5a",
-  mane_accent_active: "#14ba74",
-};
-
-const root = document.documentElement;
+const root = document.body;
+let current: Theme = Theme.None;
 
 set_theme();
 
 function set_theme() {
   const local_theme = load_theme();
-  const dark_mode = window.matchMedia("(prefers-color-scheme: dark)").matches;
-  if (local_theme === Theme.None) {
-    if (dark_mode) {
-      set_current_theme(dark_theme);
-    } else {
-      set_current_theme(light_theme);
-    }
-  } else if (local_theme === Theme.Light) {
-    set_current_theme(light_theme);
-  } else if (local_theme === Theme.Dark) {
-    set_current_theme(dark_theme);
-  }
+  set_current_theme(local_theme);
 }
 
 function save_theme(theme: Theme) {
@@ -73,30 +21,19 @@ function save_theme(theme: Theme) {
 function load_theme() {
   const theme = localStorage.getItem("theme");
   switch (theme) {
-    case "light":
+    case Theme.Light:
       return Theme.Light;
-    case "dark":
+    case Theme.Dark:
       return Theme.Dark;
     default:
       return Theme.None;
   }
 }
 
-function set_current_theme(theme: ThemeSet) {
-  root.style.setProperty("--text", theme.text);
-  root.style.setProperty("--background", theme.background);
-  root.style.setProperty("--foreground", theme.foreground);
-  root.style.setProperty("--shadow", theme.shadow);
-  root.style.setProperty("--border", theme.border);
-  root.style.setProperty("--placeholder", theme.placeholder);
-  root.style.setProperty("--input_background", theme.input_background);
-  root.style.setProperty("--button_background", theme.button_background);
-  root.style.setProperty(
-    "--button_hover_background",
-    theme.button_hover_background
-  );
-  root.style.setProperty("--mane_accent_inactive", theme.mane_accent_inactive);
-  root.style.setProperty("--mane_accent_active", theme.mane_accent_active);
+function set_current_theme(theme?: Theme) {
+  root.classList.remove(current);
+  if (theme) root.classList.add(theme);
+  current = theme ?? Theme.None;
 }
 
 export {};
